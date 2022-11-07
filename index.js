753d570524d1551af4e2309905f865d3a5bd8860
munna vail <109848258+munna-vai-xxx@users.noreply.github.com>
2022-11-08 00:37:59 +0600

    mongo db added

--- index.js
+++ index.js
@@ -17,9 +17,28 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+//MONGO DB API
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.BD_USER_PASSWORD}@cluster0.4fdxwm9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//MONGO DB FUNCTION
+async function mongoDbRun() {
+    try {
+        const collection = client.db("servicesCollection").collection("services");
+
+        //READ_OPERATION
+        app.get("/services", async (req, res) => {
+            const query = {};
+            const cursor = collection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    } finally { }
+} mongoDbRun().catch((err) => console.error(err));
+
+
 
 //APP LISTENERS
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-    console.log(uri)
 })
