8f2dccddd6a0ed53e4c70ab5d308f1bbc90f918a
devsayemmolla <98878071+devsayemmolla@users.noreply.github.com>
2022-11-10 20:00:42 +0600

    my reviews

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { query } = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,14 +24,66 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 //MONGO DB FUNCTION
 async function mongoDbRun() {
     try {
-        const collection = client.db("servicesCollection").collection("services");
+        const servicecollection = client.db("servicesCollection").collection("services");
+        const reviewsCollection = client.db("servicesCollection").collection("reviews");
 
-        //READ_OPERATION
+        //REVIEWS  CREATE ONE OPARATION(C)
+        app.post("/reviews", async (req, res) => {
+            const data = req.body;
+            console.log(data);
+            const reviews = await reviewsCollection.insertOne(data);
+            res.send(reviews);
+        });
+        // REVIEWS READ_ALL_OPERATION
+        app.get("/reviews", async (req, res) => {
+            const query = {};
+            const cursor = reviewsCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+        // REVIEWS READ_BY_EMAIL_OPERATION 
+        app.get("/myreviews", async (req, res) => {
+            console.log(req.query.email)
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    "email": req.query.email
+                }
+            }
+            const cursor = reviewsCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+
+        //SERVICES CREATE ONE  OPARATION(C)
+        app.post("/services", async (req, res) => {
+            const data = req.body;
+            console.log(data);
+            const service = await servicecollection.insertOne(data);
+            res.send(service);
+        });
+        // SERVICE READ_ALL_OPERATION 
         app.get("/services", async (req, res) => {
             const query = {};
-            const cursor = collection.find(query);
+            const cursor = servicecollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        // SERVICES READ_limit_OPERATION
+        app.get("/homeservices", async (req, res) => {
+            const query = {};
+            const cursor = servicecollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        //SERVICES READ_ONE_OPERATION
+        app.get("/services/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicecollection.findOne(query);
+            res.send(service);
         });
     } finally { }
 } mongoDbRun().catch((err) => console.error(err));
